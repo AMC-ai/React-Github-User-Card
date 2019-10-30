da3669a53bdde338d80eeba75bc61a72c577880c
AMC <53350942+AMC-ai@users.noreply.github.com>
2019-10-30 11:54:02 -0400

    updated card styles for overlapping text

--- react-github-user-card/src/components/UserCard.js
+++ react-github-user-card/src/components/UserCard.js
@@ -5,11 +5,11 @@ const UserCard = props => {
         <div className="card">
             <img src={props.user.avatar_url} />
             <div className="card-info">
-                <h3 className="name">{props.user.name}</h3>
-                <p className="user-blog"><a href={props.user.blog}>{props.user.blog}</a></p>
+                <h3 className="name">{props.user.name || 'Name'}</h3>
+                <p className="user-blog"><a href={props.user.blog}>{props.user.blog || 'LinkedIn'}</a></p>
                 <p><a href={props.user.html_url}>{props.user.html_url}</a></p>
-                <p>{props.user.location}</p>
-                <p>{props.user.bio}</p>
+                <p>{props.user.location || 'Unknown'}</p>
+                <p>{props.user.bio || 'Web Developer paving the way to greater accomplishments.'}</p>
             </div>
         </div>
     );
